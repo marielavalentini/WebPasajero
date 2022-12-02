6c26c0cbcfe3c426c2b4bfb2ce1c7d1520764190
Mariela Valentini <marielavalentini@outlook.com>
2022-12-02 11:06:29 -0300

    Formato DateTime

--- WebPasajero/Models/Pasajero.cs
+++ WebPasajero/Models/Pasajero.cs
@@ -8,6 +8,8 @@ namespace WebPasajero.Models
         public int PasajeroId { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime FechaNacimiento { get; set; }
         public string Ciudad { get; set; }
     }
